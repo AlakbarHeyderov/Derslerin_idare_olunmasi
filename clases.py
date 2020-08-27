ff7520e5d986c9cb0c5ced44442af42ac34b1f3b
alakbar289 <60427636+alakbar289@users.noreply.github.com>
2020-08-28 01:02:48 +0400

    json file elave edilmesi. Son gunun faylalrinin elave edilmesi . 4 cu gun

---
+++ clases.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self,_qrup,_ad,_soyad):
+        self.qrup = _qrup
+        self.ad = _ad
+        self.soyad = _soyad
+    def show(self):
+        print(f"{self.qrup} nomreli qrupun telebesi {self.ad} {self.soyad} xos geldiniz.")
+
+# class Json:
+#     def show(self):
+#         daxil_olanlari_qeyd_et=open("json_istifadeci_bazasi.json","a")
+#         json_data = {"istifadeciler":[
+#                 {
+#                     "group": self.qrup,
+#                     "name": self.ad,
+#                     "surname": self.soyad
+#                 }
+#             ]}
+#         daxil_olanlari_qeyd_et.write(json.dumps(json_data))
--- derslerin_idaresi.py
+++ derslerin_idaresi.py
@@ -1,12 +1,29 @@
 import webbrowser
+import json
+from clases import User
 
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 
 def start():
-    print("Bu program derslerin duzgun wekilde "
-          "idare olunmai ucun tertib edilib."
-          "Zehmet olmasa dersin heftesini ve "
+    qrup = input("Qrup adini yazin: ")
+    ad = input("Adinizi qeyd edin: ")
+    soyad = input("Soyadinizi qeyd edin: ")
+    daxil_olanlari_qeyd_et=open("json_istifadeci_bazasi.json","a")
+    json_data = {"istifadeciler":[
+                   {
+                       "group": qrup,
+                       "name": ad,
+                       "surname": soyad
+                   }
+               ]}
+    daxil_olanlari_qeyd_et.write(json.dumps(json_data))
+    User(qrup, ad, soyad).show()
+    daxil_olanlari_qeyd_et.close()
+    print()
+    print("Bu program derslerin duzgun wekilde \n"
+          "idare olunmai ucun tertib edilib.\n"
+          "Zehmet olmasa dersin heftesini ve \n"
           "gununu secin")
     print("Program 6 ci heftenden sonraki merhele"
           "ucun nezerde tutulub")
@@ -160,6 +177,74 @@ def week6_day3():
     else:
         print("xeta")
         week6_day3()
+
+
+def week6_day4():
+    emr_secilmesi = input("Secdiyiniz emri qeyd edin: ")
+    if emr_secilmesi == "1":
+        webbrowser.get(chrome_path).open(
+            'https://docs.google.com/document/d/1xWE_32xpUsuf0E5IP3jmbl0VuCK0BKbcfc1iK4jl-bk/edit#heading=h.t2d024q8l2bp')
+        print("programin islemeyine davam etmek isteyirsen? Beli/Xeyir  emri ile cavablayin")
+        islemeye_davam = input("Cavabiniz: ")
+        if islemeye_davam == "Beli":
+            hefteye_secimi()
+        elif islemeye_davam == "Xeyir":
+            print("Ugurlar!")
+        else:
+            print("Yanlis kod daxil etdiniz.")
+            hefteye_secimi()
+    elif emr_secilmesi == "2":
+        webbrowser.get(chrome_path).open(
+            "https://drive.google.com/file/d/1IzcrTRJ9imJO-jAoSndanQZGF-2_Ik94/view?usp=sharing")
+        print("programin islemeyine davam etmek isteyirsen? Beli/Xeyir  emri ile cavablayin")
+        islemeye_davam = input("Cavabiniz: ")
+        if islemeye_davam == "Beli":
+            hefteye_secimi()
+        elif islemeye_davam == "Xeyir":
+            print("Ugurlar!")
+        else:
+            print("Yanlis kod daxil etdiniz.")
+            hefteye_secimi()
+    elif emr_secilmesi == "3":
+        webbrowser.get(chrome_path).open("https://drive.google.com/drive/u/0/folders/1I6mbMrcKRCiqamPXeZrg7YTdvYlvebz9")
+    else:
+        print("xeta")
+        week6_day4()
+
+
+def week6_day5():
+    emr_secilmesi = input("Secdiyiniz emri qeyd edin: ")
+    if emr_secilmesi == "1":
+        webbrowser.get(chrome_path).open(
+            'https://docs.google.com/document/d/1xWE_32xpUsuf0E5IP3jmbl0VuCK0BKbcfc1iK4jl-bk/edit#heading=h.t2d024q8l2bp')
+        print("programin islemeyine davam etmek isteyirsen? Beli/Xeyir  emri ile cavablayin")
+        islemeye_davam = input("Cavabiniz: ")
+        if islemeye_davam == "Beli":
+            hefteye_secimi()
+        elif islemeye_davam == "Xeyir":
+            print("Ugurlar!")
+        else:
+            print("Yanlis kod daxil etdiniz.")
+            hefteye_secimi()
+    elif emr_secilmesi == "2":
+        # webbrowser.get(chrome_path).open(
+        #     "https://drive.google.com/file/d/1IzcrTRJ9imJO-jAoSndanQZGF-2_Ik94/view?usp=sharing")
+        print("Video heleki teyin olunmayib!")
+        print("programin islemeyine davam etmek isteyirsen? Beli/Xeyir  emri ile cavablayin")
+        islemeye_davam = input("Cavabiniz: ")
+        if islemeye_davam == "Beli":
+            hefteye_secimi()
+        elif islemeye_davam == "Xeyir":
+            print("Ugurlar!")
+        else:
+            print("Yanlis kod daxil etdiniz.")
+            hefteye_secimi()
+    elif emr_secilmesi == "3":
+        # webbrowser.get(chrome_path).open("https://drive.google.com/drive/u/0/folders/1I6mbMrcKRCiqamPXeZrg7YTdvYlvebz9")
+        print("Ders programi heleki teyin olunmayib!")
+    else:
+        print("xeta")
+        week6_day5()
 
 
 start()
